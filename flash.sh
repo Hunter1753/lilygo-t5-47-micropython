16e9855d82aa4ec92c156e918fbffa9fb2e35fb0
Alessandro Guttrof <hunter1753@gmail.com>
2026-03-06 04:56:37 +0100

    update flash utility and allow ci building to continue if others fail

--- flash.sh
+++ flash.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-esptool --chip esp32 -b 460800 --before default-reset --after hard-reset write-flash --flash-mode dio --flash-size 16MB --flash-freq 80m 0x1000 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x10000 build/micropython.bin
+BOARD=${1:-LILYGO_T5_47}
+
+cd micropython/ports/esp32
+idf.py -B ../../../build/$BOARD flash
